8de1580d67fad5ed12560791f245ba2561dd8beb
pietracoops <pietracoops@gmail.com>
2022-09-14 23:57:32 -0400

    fixing pypi installation

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-
+import time
 
 setup(
     name='XPlaneApi',
@@ -7,10 +7,12 @@ setup(
     license='MIT',
     author="Massimo Pietracupa",
     author_email='Massimo.Pietracupa@hotmail.com',
-    packages=find_packages('XPlane_Api'),
-    package_dir={'': 'XPlaneApi'},
+    packages=['XPlaneApi', 'examples'],
+    scripts=['examples/example1.py'],
     url='https://github.com/Pietracoops/XPlane_Python_Client',
-    keywords='example project',
+    keywords='XPlane API',
+    description='API Python Client for C++ Xplane Server',
+    long_description=open('README.rst').read(),
     install_requires=[
           'pyzmq==23.2.1',
           'zmq'
